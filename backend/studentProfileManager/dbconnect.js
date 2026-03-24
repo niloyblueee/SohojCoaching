5ca518ed5c2121b154915479c9093d646e08082b
Oishowrjyo <156098850+oishowrjyo@users.noreply.github.com>
2026-03-24 13:41:31 +0600

    edited

--- backend/studentProfileManager/dbconnect.js
+++ backend/studentProfileManager/dbconnect.js
@@ -2,7 +2,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "NKCeddCdkqDFmJMTZeEXoSsBaebeUeVW",
+    //password: ___,
     host: "hopper.proxy.rlwy.net",
     port: 58469,
     database: "railway",
